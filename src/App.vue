5b10359ec1f888c80146fdde59c9ef762b68eefe
mahmoudyusof <mahmoud.yusof27@gmail.com>
2020-07-02 19:36:35 +0200

    merge sort functionality and most of animation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,154 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav>
+      <button @click="shuffle">Shuffle</button>
+      <button @click="mergeSort(arr, 0)">Merge</button>
+      <button>Bubble</button>
+      <button>Insertion</button>
+      <button>Quick</button>
+    </nav>
+    <div class="container">
+      <div class="array">
+        <app-el
+          v-for="(h, i) in arr"
+          :state="state[i]"
+          :width="elWidth"
+          :height="h"
+          :pos="i"
+          :key="i"
+        ></app-el>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Element from "./components/Element.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    "app-el": Element
+  },
+  created() {
+    const steps = Math.floor(this.width / this.elWidth);
+    const step_height = Math.floor(this.height / steps);
+    for (let i = 1; i <= steps; i++) {
+      this.arr.push(i * step_height);
+      this.state.push(0);
+    }
+    this.shuffle();
+  },
+  data() {
+    return {
+      arr: [],
+      state: [],
+      width: 800,
+      height: 500,
+      elWidth: 5
+    };
+  },
+  methods: {
+    shuffle() {
+      for (let i = this.arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = this.arr[i];
+        this.$set(this.arr, i, this.arr[j]);
+        this.$set(this.arr, j, temp);
+        this.$set(this.state, i, 0);
+      }
+      this.$set(this.state, 0, 0);
+    },
+    async mergeSort(ar, start) {
+      if (ar.length == 1) {
+        return ar;
+      } else {
+        let mid = Math.floor(ar.length / 2);
+        let left = await this.mergeSort(ar.slice(0, mid), start);
+        let right = await this.mergeSort(ar.slice(mid), mid + start);
+        for (let i = 0; i < this.arr.length; i++) {
+          if (i >= start && i < start + ar.length) {
+            this.$set(this.state, i, 1);
+          } else {
+            this.$set(this.state, i, 0);
+          }
+        }
+        let s = this.merge(left, right, start, mid + start);
+        await this.populate(s, start);
+        return s;
+      }
+    },
+    merge(a, b, start, mid) {
+      let res = [];
+      while (a.length && b.length) {
+        if (a[0] >= b[0]) {
+          res.push(b[0]);
+          b.shift();
+        } else {
+          res.push(a[0]);
+          a.shift();
+        }
+      }
+      if (!a.length) {
+        res = res.concat(b);
+      } else if (!b.length) {
+        res = res.concat(a);
+      }
+      return res;
+    },
+    async populate(ar, start) {
+      for (let i = 0; i < ar.length; i++) {
+        this.$set(this.arr, start + i, ar[i]);
+        await this.sleep(10);
+      }
+    },
+    sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.container {
+  background-color: #eee;
+  height: 600px;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.array {
+  height: 500px;
+  width: 800px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  position: relative;
+}
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #222;
+  padding: 0 20px;
+}
+nav button {
+  background-color: #222;
+  border: none;
+  padding: 20px 30px;
+  color: #ddd;
+  cursor: pointer;
+  transition: ease all 0.2s;
+}
+nav button:hover {
+  background-color: black;
+  color: white;
 }
 </style>
---
+++ src/components/Element.vue
@@ -0,0 +1,32 @@
+<template functional>
+  <div
+    class="el"
+    :style="{
+      width: `${props.width}px`,
+      height: `${props.height}px`,
+      left: `${props.pos * props.width + 1}px`,
+      'background-color': props.state ? '#4fc08d' : '#2c3e50'
+    }"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: "app-el",
+  props: {
+    height: Number,
+    width: Number,
+    pos: Number,
+    state: Number
+  }
+};
+</script>
+
+<style>
+.el {
+  background-color: #2c3e50;
+  border-right: 1px solid white;
+  position: absolute;
+  bottom: 0;
+}
+</style>
